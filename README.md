9aa9fb1cb4862fb5ca43bf1a8c97963838142c2e
Katie Miller <katie@codemiller.com>
2014-01-30 21:38:15 +1000

    Adding first version of script

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-revealation
+A script to help generate PDF versions of Reveal.js HTML slide decks
+=======
+Revealation
 ===========
 
-A script to help generate PDF versions of Reveal.js HTML slide decks
+This is a script to help capture a [Reveal.js](https://github.com/hakimel/reveal.js) presentation and turn it into a PDF, which can then be uploaded to sites such as [SlideShare](www.slideshare.net). If the presentation you wish to convert is simple, you may find you get acceptable output by adding the _print-pdf_ query parameter to the presentation URL, as described in the Reveal.js docs, and printing to PDF that way. If however, your presentation has content that is overlayed on other content and revealed with fragments and/or you wish to show the fragments on your slides one by one in the PDF, this script may provide a better solution.
+
+- Install [PhantomJS](http://phantomjs.org) and make sure the executable is on your path 
+- Install [ImageMagick](www.imagemagick.org) and make sure the `convert` command is available on your path 
+- Run the command `phantomjs revealation.js http://revealurl.example.com`, replacing the URL with your own 
+- Look at the PNG files in the directory and delete any unwanted ones; every Reveal.js fragment will be captured
+- Run the command `convert *.png slides.pdf`
+
---
+++ revealation.js
@@ -0,0 +1,130 @@
+var page = require('webpage').create(),
+    system = require('system');
+
+if (system.args.length === 1) {
+    console.log('Pass the Reveal.js presentation URL as an argument, eg: phantomjs revealation.js http://lab.hakim.se/reveal-js');
+    phantom.exit(1);  
+}
+
+var url = system.args[1];
+var maxSlideIndex = system.args[2] || '999';
+
+page.open(url, function(status) {
+    if (status !== 'success') {
+        console.log('Unable to load Reveal.js presentation at URL: ' + url);
+        phantom.exit(1);
+    }
+
+    dims = getSlideDimensions(page);
+    page.viewportSize = { width: dims.width, height: dims.height };
+    page.paperSize = { width: dims.width, height: dims.height, margin: '0px' }
+
+    page.evaluate(function() {
+        Reveal.configure({ controls: false, 
+                           transition: 'none',
+                           rollingLinks: false });
+    });
+
+    for (var slidesToGo = true; slidesToGo; slidesToGo = !isLastSlide(page)) { 
+        var slideIndices = getSlideIndices(page); 
+        var slideId = padWithZeroes(slideIndices.h) + '-' + padWithZeroes(slideIndices.v); 
+
+        renderFrame(page, slideId + '-' + padWithZeroes(0));
+
+        if (hasFragments(page)) {
+            processFragments(page, slideId);
+        } 
+
+        nextSlide(page);
+    }
+
+  phantom.exit();
+});
+
+var processFragments = function(page, slideId) {
+    for (var fragmentsToGo = true; fragmentsToGo; fragmentsToGo = hasNextFragment(page)) {
+        nextFragment(page);
+        //TODO Add back CSS transition handling code here
+        var fragmentId = getFragmentIndex(page);
+        renderFrame(page, slideId + '-' + padWithZeroes(fragmentId));
+    }   
+}
+
+var renderFrame = function(page, id) {
+    page.render('frame-' + id + '.png'); 
+}
+
+var doEval = function(page, func) {
+    return page.evaluate(func);
+} 
+
+var forceRepaint = function(page) {
+    doEval(page, function() { 
+        // Hack from http://stackoverflow.com/questions/3485365/how-can-i-force-webkit-to-redraw-repaint-to-propagate-style-changes
+        var curSlide = document.getElementsByClassName('present')[0];
+        curSlide.style.display = 'none';
+        curSlide.offsetHeight;
+        curSlide.style.display = 'block';
+    });
+}
+
+var isLastSlide = function(page) {
+    return doEval(page, function() {
+        return Reveal.isLastSlide();
+    });
+}
+
+var hasFragments = function(page) {
+    var fragments = getSlideFragments(page);
+    return fragments.prev || fragments.next; 
+}
+
+var hasNextFragment = function(page) {
+    var fragments = getSlideFragments(page);
+    return fragments.next; 
+}
+
+var nextSlide = function(page) {
+    doEval(page, function() {
+        Reveal.next();
+    });
+}
+
+var nextFragment = function(page) {
+    doEval(page, function() {
+        Reveal.nextFragment();
+    });
+    forceRepaint(page);
+}
+
+var getSlideDimensions = function(page) {
+    return doEval(page, function() {
+        return { width: Reveal.getConfig().width,
+                 height: Reveal.getConfig().height }
+    });
+}
+
+var getSlideFragments = function(page) {
+    return doEval(page, function() {
+        return Reveal.availableFragments();
+    });
+}
+
+var getSlideIndices = function(page) {
+    return doEval(page, function() {
+        return Reveal.getIndices();
+    });
+}
+
+var getFragmentIndex = function(page) {
+    return getSlideIndices(page).f;
+}
+
+var padWithZeroes = function(num) {
+    var numDigits = maxSlideIndex.toString().length; 
+    if (num <= maxSlideIndex) { 
+        num = (Array(numDigits).join("0") + num).slice(-numDigits) 
+    }
+    return num;
+}
+
